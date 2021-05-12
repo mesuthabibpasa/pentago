e3a68e63acbbdbdb532bb65a6b9d1256fc0c47b5
Goktug-Turan <goktugturan16@gmail.com>
2021-05-12 19:35:44 +0300

    it is messed up

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const _ = require('lodash')
-// const prompt = require('prompt')
-var readlineSync = require('readline-sync')
+const prompt = require('prompt')
 const { rotate90, rotate270 } = require('2d-array-rotation')
 
 const KEYS_IN_A_ROW_WIN_LIMIT = 5
 const dim = 3
 const getMiniBoard = () => {
   const mini = []
-  for (let r = 0; r < dim; r += 1) {
+  for (let r = 0; r < dim; r+=1) {
     const inner = []
-    for (let c = 0; c < dim; c += 1) {
+    for (let c = 0; c < dim; c+=1) {
       inner.push(null)
     }
     mini.push(inner)
@@ -42,11 +41,7 @@ const getBoard = () => {
 
 
 const putPiece = (piece, mini, rowIndex, colIndex) => {
-  if (mini[rowIndex][colIndex] === null){
-    mini[rowIndex][colIndex] = piece;
-  } else {
-    return 'occupied'
-  }
+  mini[rowIndex][colIndex] = piece;
 }
 
 const rotateMini = (mini, isCCW = false) => {
@@ -60,26 +55,25 @@ const rotateMini = (mini, isCCW = false) => {
 }
 
 const action = (piece, miniToPutPiece, rowIndex, colIndex, miniToRotate, ccw) => {
-  let isOccupied = putPiece(piece, miniToPutPiece, rowIndex, colIndex);
-  if (isOccupied) return isOccupied
-  const winnerAfterPutPiece = isGameOver()
-  if (winnerAfterPutPiece) {
-    return winnerAfterPutPiece
-  }
-  rotateMini(miniToRotate, ccw);
-  const winnerAfterRotateMini = isGameOver()
-  if (winnerAfterRotateMini) {
-    return winnerAfterRotateMini
-  }
+    putPiece(piece, miniToPutPiece, rowIndex, colIndex);
+    const winnerAfterPutPiece = isGameOver()
+    if (winnerAfterPutPiece) {
+      return winnerAfterPutPiece
+    }
+    rotateMini(miniToRotate, ccw);
+    const winnerAfterRotateMini = isGameOver()
+    if (winnerAfterRotateMini) {
+      return winnerAfterRotateMini
+    }
 
-  return null
+    return null
 };
 
 const isGameOver = () => {
   const winners = []
   const board = getBoard();
-  for (let r = 0; r < board.length; r += 1) {
-    for (let c = 0; c < board[0].length; c += 1) {
+  for (let r = 0; r < board.length; r+=1) {
+    for (let c = 0; c < board[0].length; c+=1) {
       const winner = search(board, r, c)
       if (winner) {
         winners.push(winner)
@@ -135,69 +129,67 @@ const searchInDirection = (board, r, c, keysInARow, dir) => {
   return null
 }
 
-search(getBoard(), 0, 0)
 
-async function init() {
-  let winner = null
-  // prompt.start();
-  console.log('Game is On!')
+
+async function init () {
+  let winner = null;
   const players = [1, -1]
-  const boardsArray = ['A', 'B', 'C', 'D']
   let turn = 0
-  while (winner === null || winner === 'occupied') {
     // const { userAction } = await prompt.get(['userAction']);
     // const [boardToPutPiece, rIndex, cIndex, boardToRotate, isCCW] = userAction.split('_')
-    const turnMessage = winner === 'occupied' ? 'This place is already occupied please try another place' : `Your move ${players[turn % 2]}`
-    console.log(turnMessage)
-    let putPieceBoardIndex = readlineSync.keyInSelect(boardsArray, 'Which board to put piece? ', {
-      guide: false,
-      cancel: false,
-    })
-    let rowIndex = readlineSync.questionInt(`Which row of ${boardsArray[putPieceBoardIndex]}: `)
-    let colIndex = readlineSync.questionInt(`Which column of ${boardsArray[putPieceBoardIndex]}: `)
-    while (rowIndex < 0 || rowIndex > 2 || colIndex < 0 || colIndex > 2){
-      console.log(`Please give valid numbers up to ${dim - 1}`)
-      rowIndex = readlineSync.questionInt(`Which row of ${boardsArray[putPieceBoardIndex]}: `, {
-        min: 0,
-        max: 2,
-      })
-      colIndex = readlineSync.questionInt(`Which column of ${boardsArray[putPieceBoardIndex]}: `, {
-        min: 0,
-        max: 2,
-      })
-    }
-      let rotateBooardIndex = readlineSync.keyInSelect(boardsArray, 'Which board to rotate? ', {
-        guide: false,
-        cancel: false,
-      })
-    if (readlineSync.keyInYN('Do you want turn CCW? Y: Yes N: No')) {
-      // 'Y' key was pressed.
-      isCCW = true
-    } else {
-      // Another key was pressed.
-      isCCW = false
-    }
+    // winner = action(players[turn % 2], BOARD[boardToPutPiece], +rIndex, +cIndex, BOARD[boardToRotate], isCCW === 'true')
+    var schema = {
+      properties: {
+        boardToPutPiece: {
+          description: 'Which board do you want to put your piece on? (options are: A, B, C, D)',
+          type: 'string',
+          // message: 'You should choose one of the options mentioned above.',
+          // required: true,
+        }, 
+        rIndex: {
+          description: 'Which row do you want to put your piece on? (options are: 0, 1, 2)',
+          type: 'number',
+          // message: 'You should choose one of the options mentioned above.',
+          // required: true,
+        }, 
+        cIndex: {
+          description: 'Which cloumn do you want to put your piece on? (options are: 0, 1, 2)',
+          type: 'number',
+          // message: 'You should choose one of the options mentioned above.',
+          // required: true,
+        }, 
+        boardToRotate: {
+          description: 'Which board do you want to rotate? (options are: A, B, C, D)',
+          type: 'string',
+          // message: 'You should choose one of the options mentioned above.',
+          // required: true,
+        }, 
+        isCCW: {
+          description: 'Do you want to rotate the board counter clockwise? (options are: true or false)',
+          type: 'boolean',
+          // message: 'You should choose one of the options mentioned above.',a
+          // required: false,
+        }
+      } 
+    };
+    prompt.start();
+    prompt.get(schema, function (err, result) {
+      winner = action(players[turn % 2], BOARD[result.boardToPutPiece], result.rIndex, result.cIndex, BOARD[result.boardToRotate], result.isCCW === 'true');
+      console.log(getBoard())
+    });
 
-    winner = action(players[turn % 2], BOARD[boardsArray[putPieceBoardIndex]], +rowIndex, +colIndex, BOARD[boardsArray[rotateBooardIndex]], isCCW)
-    console.log(getBoard())
-    if(winner !== 'occupied'){
-      turn++
-    }
-  }
+    turn++
 
   if (winner === 0) {
     console.log(`It is a draw`)
-  } else {
+  } if(winner === 1 || winner === -1) {
     console.log(`Player ${winner} won in ${turn} turns!`)
   }
 }
 
-try {
-  init()
-} catch (err) {
-  console.log('something went wrong!');
-}
+init()
 
-// Only accept valid userActions 
+// Only accept valid userActions
 // Better interface for entering user actions
 // If user tries to put piece on a non-empty space, it should fail
+
